7863b4ed8dafdf78b64b5c56952bc6c58d24f82b
Nattaphon <66070280@kmitl.ac.th>
2026-09-24 19:32:29 +0700

    1

---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,13 @@
+import sys
+params = sys.argv[1:]
+
+if len(params) != 2:
+    print("none")
+else:
+    start = int(params[0])
+    end = int(params[1])
+    if start <= end:
+        result = list(range(start, end + 1))
+    else:
+        result = list(range(start, end - 1, -1))
+    print(result)
